eba3cf9d2edbe66795e23fcebcdd45a599bff683
Miguel Marcondes <miguelmarcondesfilho@gmail.com>
2020-03-05 19:29:41 -0300

    Adding connection to dollar api, pokemon api and showing pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ this.dolar }}</p>
+    <p>{{ this.pokemonId }}</p>
+    <p>{{ this.pokemon.name }}</p>
+    <img :src="this.pokemon.sprites.front_default" alt="">
+    <img :src="this.pokemon.sprites.back_default" alt="">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      dolar: '',
+      pokemon: {},
+      pokemonId: ''
+    }
+  },
+  created: function () {
+    axios.get('http://economia.awesomeapi.com.br/jsonp/USD-BRL')
+      .then((result) => {
+        this.dolar = Number(result.data[0].high).toFixed(2)
+        this.pokemonId = this.dolar.replace('.', '')
+
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${this.pokemonId}`)
+          .then((result) => {
+            console.log(result)
+            this.pokemon = result.data
+          })
+      })
   }
 }
 </script>
